7bc1bbe6ee79ad4180905c304c7d9fd79e87c97a
Temi <temi@Temis-MacBook-Pro.local>
2021-01-01 20:55:26 +0000

    add firebase to project

--- src/index.js
+++ src/index.js
@@ -8,8 +8,23 @@ import { Provider } from 'react-redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react'
 import storage from 'redux-persist/lib/storage';
+import firebase from 'firebase';
+import "firebase/firestore";
 import rootReducer from './store/rootReducer';
 import App from './App';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArMVNWhoG9LhcjU_UvUTFo0r-0XGVlXDk",
+  authDomain: "six-task.firebaseapp.com",
+  databaseURL: "https://six-task.firebaseio.com",
+  projectId: "six-task",
+  storageBucket: "six-task.appspot.com",
+  messagingSenderId: "28885894950",
+  appId: "1:28885894950:web:e30d98fcfbcf96ba8323e7",
+  measurementId: "G-QJLXFXG8JZ"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const log = createLogger({ diff: true, collapsed: true });
 const middlewares = [thunk, log];
@@ -24,7 +39,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(persistedReducer, compose(applyMiddleware(...middlewares), ...enhancers));
 
-const persistor = persistStore(store)
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/services/store.service.js
@@ -0,0 +1,14 @@
+class StoreService {
+  saveTasks() {}
+  getTasks() {}
+  getTask() {}
+  saveBoard() {}
+  getBoards() {}
+  getBoard() {}
+  saveList() {}
+  getLists() {}
+  getList() {}
+}
+
+const storeService = new StoreService();
+export default storeService;
